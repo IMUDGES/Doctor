691943b1e65bbc840735e9ab58a47c5fc5e9b508
HanHaoYu <hanhaoyu@imudges.com>
2016-07-30 16:18:53 +0800

    change

--- php/common_register.php
+++ php/common_register.php
@@ -5,21 +5,16 @@
  * Date: 2016/7/29
  * Time: 10:59
 */
-header("Content-type:text/html;charset=utf8");
-$sql="INSERT INTO common(username,email,password)
-VALUES('$_GET[name]','$_GET[email]','$_GET[password]')";
+$sql="INSERT INTO common(username,email,password)VALUES('$_GET[name]','$_GET[email]','$_GET[password2]');";
 $mysqli = new mysqli('123.207.141.93', 'root', 'a96S04d02', 'user');
-if($mysqli->query($sql)==TRUE)
+if($mysqli->query($sql)!=TRUE)
 {
-    echo "user entry saved successfully";
+    echo "INSERT successsful";
 }
 else
 {
     echo "INSERT attempt failed";
 }
 $mysqli->close();
-/*echo $_GET["name"];
-echo $_POST["email"];
-echo $_POST["password1"];
-echo $_post["password2"];*/
+header('http://localhost:63342/Doctor/mian.html');
 ?>
