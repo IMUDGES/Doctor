9acf840405534d204762bec2caa938b88854bf36
HanHaoYu <hanhaoyu@imudges.com>
2016-08-03 09:20:18 +0800

    change
    写完了医生注册

--- php/doctor_register.php
+++ php/doctor_register.php
@@ -1,17 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wodediannao
- * Date: 2016/7/29
- * Time: 10:59
-*/
 header('Content-Type:text/html;charset=utf-8');
-$sql="INSERT INTO common(username,email,password)VALUES('$_GET[name]','$_GET[email]','$_GET[password2]');";
+$sql="INSERT INTO doctor(d_username,d_email,d_phonenumber,d_type,d_introduce,d_password) 
+VALUES('$_GET[name]','$_GET[email]','$_GET[phonenumber]','$_GET[type]','$_GET[introduce]','$_GET[password1]');";
 $mysqli = new mysqli('123.207.141.93', 'root', 'a96S04d02', 'user');
-$res="SELECT * FROM common where username='{$_GET['name']}';";
+$res="SELECT * FROM doctor where d_username='{$_GET['name']}';";
 $result = mysqli_query($mysqli,$res);
 $row = mysqli_fetch_row($result);
-mysqli_query($mysqli,"SET NAMES `UTF-8`");
 if($row) {
     echo "failed";
 }
@@ -21,4 +15,5 @@ else {
     }
 }
 header('Location:http://localhost:63342/Doctor/mian.html?_ijt=j29jfjd8p6bb1ec960u1ltcmi3');
+
 ?>
